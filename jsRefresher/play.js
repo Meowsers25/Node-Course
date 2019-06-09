b2cd5237e55934dc51a2cfb11e21a99c00ed7635
Chris <c.lamothe25@gmail.com>
2019-06-08 20:51:00 -0400

    let & const / arrow functions

--- jsRefresher/play.js
+++ jsRefresher/play.js
@@ -1,11 +1,21 @@
-var name = "Chris";
+const name = "Chris";
 var age = 47;
-var hasHobbies = true;
+const hasHobbies = true;
 
-function user(name, age, hasHobbies) {
+// examples of arrow functions
+const user = (name, age, hasHobbies) => {
   return (
     "User " + name + " is " + age + " years old. Has hobbies: " + hasHobbies
   );
-}
+};
 
 console.log(user(name, age, hasHobbies));
+
+// same line when only using one return statement
+const add = (a, b) => a + b;
+
+console.log(add(2, 3));
+
+// if you use only 1 parameter, no parens
+const addOne = a => a + 1;
+console.log(addOne(3));
