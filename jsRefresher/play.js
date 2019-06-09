50ab90f8472b74b94af9d699fea056dc45db1769
Chris <c.lamothe25@gmail.com>
2019-06-08 20:10:35 -0400

    Initial commit; Starting with JS refresher

---
+++ jsRefresher/play.js
@@ -0,0 +1,11 @@
+var name = "Chris";
+var age = 47;
+var hasHobbies = true;
+
+function user(name, age, hasHobbies) {
+  return (
+    "User " + name + " is " + age + " years old. Has hobbies: " + hasHobbies
+  );
+}
+
+console.log(user(name, age, hasHobbies));
